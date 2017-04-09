600b19dd9b8521ecd8e46c4207ad29215566d375
worthingtont12 <worthingtont12@gmail.com>
2017-04-09 12:17:21 -0400

    test query

---
+++ test_query.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pandas.io.sql as psql
+import psycopg2
+
+conn = psycopg2.connect(
+    "dbname='dbsys6016' user=%s host=%s password=%s" % (user, host, password))
+
+primary = psql.read_sql(
+    "SELECT * FROM usa_primary", conn)
