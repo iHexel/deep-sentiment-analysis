b26d65150c98c671388c5d0df78e066bd1024bc6
worthingtont12 <worthingtont12@gmail.com>
2017-04-09 13:56:32 -0400

    delete test query

--- test_query.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pandas.io.sql as psql
-import psycopg2
-
-conn = psycopg2.connect(
-    "dbname='dbsys6016' user=%s host=%s password=%s" % (user, host, password))
-
-primary = psql.read_sql(
-    "SELECT * FROM usa_primary", conn)
