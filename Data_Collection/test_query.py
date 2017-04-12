f9cffe9aa046b98823cfd4dbdfa9cc0c1279706d
worthingtont12 <worthingtont12@gmail.com>
2017-04-12 15:43:04 -0400

    test query update

--- Data_Collection/test_query.py
+++ Data_Collection/test_query.py
@@ -5,9 +5,5 @@ import psycopg2
 conn = psycopg2.connect(
     "dbname='dbsys6016' user=%s host=%s password=%s" % (user, host, password))
 
-    = psql.read_sql(
-        "SELECT * FROM usa_primary", conn)
-filter Rt
-should we Rt?
-x = primary[primary['source'].str.contains("google")]
-primary['user_lang'].value_counts()
+primary = psql.read_sql(
+    "SELECT * FROM usa_primary", conn)
